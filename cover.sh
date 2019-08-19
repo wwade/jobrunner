b8deda919ce68e03dd6a4c4d846eb11e63d9af5f
wwade <wade.carpenter+github@gmail.com>
2019-08-19 10:55:35 -0700

    cover.sh: accept additional args for nosetests

--- cover.sh
+++ cover.sh
@@ -2,5 +2,5 @@
 pip install --upgrade nose-cov
 COVDIR="$HOME/tmp/jobrunner-coverage"
 mkdir -p "$COVDIR"
-nosetests -v -e integration --cover-erase --cover-inclusive --with-coverage --cover-html-dir="$COVDIR" --cover-html
-echo "Coverage report dir: ${COVDIR}"
+nosetests -v -e integration --cover-erase --cover-inclusive --with-coverage --cover-html-dir="$COVDIR" --cover-html "$@"
+echo "Coverage report dir: ${COVDIR}/index.html"
