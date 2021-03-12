a5f0e17702e6ab6f60fad96016a847b36f3aa44c
Wade Carpenter <wade@arista.com>
2021-03-11 23:56:03 -0800

    env: make autopep8 happy with explicit --indent-size
    For some reason it suddenly complains about some imaginary indent-size option in
    pycodestyle, seemingly coming from autopep8. :shrugs:

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -xe
-FILESPEC="job setup.py jobrunner"
-isort -y -ac -rc $FILESPEC
-autopep8 -j32 -rai $FILESPEC
-pylint $FILESPEC
+FILESPEC=(job setup.py jobrunner)
+isort -y -ac -rc "${FILESPEC[@]}"
+autopep8 -j32 -rai --indent-size=4 "${FILESPEC[@]}"
+pylint "${FILESPEC[@]}"
