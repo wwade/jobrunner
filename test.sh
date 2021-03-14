60b1033f433a7b2146a0989896314a9a45bbed3a
Wade Carpenter <wwade@users.noreply.github.com>
2021-03-14 00:43:09 -0800

    Helper script to run tests from fresh environment

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "$(dirname $0)"
+set -xeuo pipefail
+git clone /src /work
+cd /work
+PY=$(python -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))")
+ln -svf Pipfile-"${PY}".lock Pipfile.lock
+python -m pip install --upgrade pip setuptools wheel pipenv
+pipenv --python "$PY" install --dev
+
+FILES=(job setup.py jobrunner)
+pipenv run isort -c --diff -rc "${FILES[@]}"
+pipenv run autopep8 --exit-code -ra --diff "${FILES[@]}"
+pipenv run pylint -d fixme "${FILES[@]}"
+
+pipenv run pip install .
+
+pipenv run pytest -v -l --junitxml=junit/test-results.xml --durations=10 jobrunner/
