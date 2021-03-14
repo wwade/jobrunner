9c4fb87f2d2ce89bc69a04a1dda9ce15aef42217
wwade <wwade@users.noreply.github.com>
2021-03-14 13:35:22 -0700

    db: Remove DBM-based DB for py3 support
    Switched over to sqlite-backed JSON storage a while back, so strip out the dbm
    version since it doesn't play nicely in py3-land.

--- jobrunner/service/registry.py
+++ jobrunner/service/registry.py
@@ -1,14 +1,12 @@
+from __future__ import absolute_import
+
 from . import service
 from ..db import JobInfo
-from ..db.dbm_db import DbmJobs
 from ..db.sqlite_db import Sqlite3Jobs
 
 
-def registerServices(sqlite=False, testing=False):
+def registerServices(testing=False):
     if testing:
         service().clear(thisIsATest=testing)
-    if sqlite:
-        service().register("db.jobs", Sqlite3Jobs)
-    else:
-        service().register("db.jobs", DbmJobs)
+    service().register("db.jobs", Sqlite3Jobs)
     service().register("db.jobInfo", JobInfo)
