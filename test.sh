3dfa3821181c475e3c32108aaa6d369514e0ae57
wwade <wwade@users.noreply.github.com>
2025-11-18 16:23:41 -0800

    wip poetry

--- test.sh
+++ test.sh
@@ -2,32 +2,29 @@
 cd "$(dirname "$0")" || exit 1
 set -xeuo pipefail
 PY=$(python -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))")
-python -m pip install --upgrade pip setuptools wheel pipenv
-set +x
-mirrorUrl=$(pip config list | grep index-url | cut -d= -f2 | cut -d\' -f2 || true)
-mirror=()
-if [[ -n "$mirrorUrl" ]]; then
-    mirrorUrl=${mirrorUrl/[[:space:]]/}
-    echo "mirrorUrl: $mirrorUrl"
-    mirror+=(--pypi-mirror="$mirrorUrl")
+
+# Check if poetry is installed
+if ! command -v poetry &> /dev/null; then
+    echo "Poetry not found, installing..."
+    python -m pip install --upgrade pip
+    python -m pip install poetry
 fi
-echo mirror: "${mirror[@]}"
-set -x
 
-pipenv=(python -m pipenv)
+poetry --version
 
-"${pipenv[@]}" --python "$PY" "${PIPENV_CMD:-sync}" "${mirror[@]}" --dev
+# Install dependencies
+poetry install --all-extras
 
 FILES=(setup.py jobrunner)
 if [[ "$PY" =~ 3\. ]]
 then
     FILES+=("test-docker.py")
 fi
-"${pipenv[@]}" run isort -c --diff -rc "${FILES[@]}"
-"${pipenv[@]}" run autopep8 --exit-code -ra --diff "${FILES[@]}"
-"${pipenv[@]}" run pylint -d fixme "${FILES[@]}"
-find jobrunner -type f -name "*.py" -not -name "compat.py" | xargs "${pipenv[@]}" run flake8 setup.py
+poetry run isort -c --diff -rc "${FILES[@]}"
+poetry run autopep8 --exit-code -ra --diff "${FILES[@]}"
+poetry run pylint -d fixme "${FILES[@]}"
+find jobrunner -type f -name "*.py" -not -name "compat.py" | xargs poetry run flake8 setup.py
 
-"${pipenv[@]}" run pip install .
+poetry run pip install .
 
-"${pipenv[@]}" run pytest -v -l --junitxml=junit/test-results.xml --durations=10 jobrunner/
+poetry run pytest -v -l --junitxml=junit/test-results.xml --durations=10 jobrunner/
